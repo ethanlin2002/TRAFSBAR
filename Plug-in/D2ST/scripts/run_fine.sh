719aea5c7ca4d13416ed974442cee1f74da82dcb
ethanlin2002 <ethanlin2002@gmail.com>
2026-04-30 15:33:25 +0800

    Update run_fine.sh

--- Plug-in/D2ST/scripts/run_fine.sh
+++ Plug-in/D2ST/scripts/run_fine.sh
@@ -1,5 +1,5 @@
 python runs/run.py \
-    --cfg config/finesports/5-shot-train-0.3.yaml
+    --cfg config/finesports/5-shot-train.yaml
 
 python runs/run.py \
-    --cfg config/finesports/5-shot-test-0.3.yaml
+    --cfg config/finesports/5-shot-test.yaml
